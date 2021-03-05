0e9096bda0fda79372cf20f98d90a2d40af3ace5
Katelyn Peterson <katelynrpeterson16@gmail.com>
2021-03-05 04:27:14 +0000

    Automatically backed up by Learn

---
+++ Testing.rb
@@ -0,0 +1,7 @@
+counter = 0
+array = [1, 2, 3, 4, 5]
+
+while array[counter] do
+  puts array[counter]
+  counter += 1
+end
---
+++ greeting.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Song
+ 
+  @@all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def self.print_all_song_names
+    @@all.split("")
+  end 
+end
